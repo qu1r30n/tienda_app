af9218d20729cf26440ac179bd2353737bb12f4c
qu1r30n <qu1r30n@hotmail.com>
2022-10-10 11:03:12 -0500

    arregle problema inventario

--- tienda_app2/pantallas/inventario.xaml.cs
+++ tienda_app2/pantallas/inventario.xaml.cs
@@ -34,22 +34,22 @@ namespace tienda_app2.pantallas
 
                 string direccion_inv = directorio + "inf/inventario/invent.txt";
                 //id_0|producto_1|precio_de_venta_2|0_3|cantidad_4|costo_compra_5|provedor_6|grupo_7|multiusos_8|cantidad_productos_por_paquete_9|
-                string nombre_produc = bas.Seleccionar(direccion_inv, 3, txt_codigo.Text, "3");
+                string nombre_produc = bas.Seleccionar(direccion_inv, 3, txt_codigo.Text, "1");
                 if (nombre_produc == "")
                 {
-                    while (nombre_produc=="")
+                    while (nombre_produc == "" || nombre_produc == (null)) 
                     {
                         nombre_produc = await DisplayPromptAsync("nombre del producto", "nombre del producto: ");
                     }
                 }
                 string cantidad_texto = "";
 
-                while (cantidad_texto == "")
+                while (cantidad_texto == "" || cantidad_texto == (null))
                 {
                     cantidad_texto = await DisplayPromptAsync("cantidad", "cantidad: ", keyboard: Keyboard.Numeric);
                 }
 
-                if (cantidad_texto != "")
+                if (cantidad_texto != "" && cantidad_texto != "-")
                 {
                     
                         mod.inventario(txt_codigo.Text, cantidad_texto, nombre_produc);
